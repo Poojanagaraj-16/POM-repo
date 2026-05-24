3347ede0ba695b4858a5ed293907a407096a74c7
Poojanagaraj-16 <96102810+Poojanagaraj-16@users.noreply.github.com>
2026-05-24 11:33:33 +0530

    Delete tests/AutosuggestDropdown.spec.js

--- tests/AutosuggestDropdown.spec.js
+++
@@ -1,6 +0,0 @@
-const {test,expect}= require('@playwright/test')
-test('AutosuggestonDropDown' , async({page})=>{
-    //await page.goto('https://www.redbus.in/')
-    //await page.locator('#src').fill('Delhi');
-    
-})
